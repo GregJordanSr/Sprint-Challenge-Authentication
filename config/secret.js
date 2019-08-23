e2b570473938687732f4a28c3f9fa648e75cfef9
Greg Jordan <gregljordan@gmail.com>
2019-08-23 12:36:30 -0400

    middleware completed and jokes are seen

--- config/secret.js
+++ config/secret.js
@@ -1,3 +1,3 @@
 module.exports = {
-    secret: process.env.JWT_SECRET || "It won't be a secret if I pass this sprint"
+    jwtSecret: process.env.JWT_SECRET || "It won't be a secret if I pass this sprint"
 };
